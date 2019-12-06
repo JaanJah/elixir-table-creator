351b92f63286c6380fce1db346c6179b14eda317
JaanJahimees <jaanjahilo@outlook.com>
2019-12-06 19:37:36 +0200

    Initial commit

---
+++ lib/elixir_table_creator.ex
@@ -0,0 +1,18 @@
+defmodule ElixirTableCreator do
+  @moduledoc """
+  Documentation for ElixirTableCreator.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirTableCreator.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_table_creator_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirTableCreatorTest do
+  use ExUnit.Case
+  doctest ElixirTableCreator
+
+  test "greets the world" do
+    assert ElixirTableCreator.hello() == :world
+  end
+end
